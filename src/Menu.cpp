79e0e86e80da1543816b29dd8e16544e655ea5d9
Diogo Vieira <145665382+DiogoSV7@users.noreply.github.com>
2024-05-19 19:49:39 +0100

    Update Menu.cpp

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -387,21 +387,20 @@ void Menu::drawBacktracking() {
         cout << "└──────────────────────────────────────────────────┘" << endl;
         waitForEnter();
     } else {
+        cout << "┌─ Backtracking Algorithm  ────────────────────────┐" << endl;
+        cout << "│" << setw(53) << "│" << endl;
+        cout << "│ " << left << setw(12) << "Tour cost: " << right << left << setw(36) << data_.getCost() << right << " │" << endl;
+        cout << "│ " << left << setw(12) << "Tour size: " << right << left << setw(36) << data_.getBestTour().size() << right << " │" << endl;
+        cout << "│ " << left << setw(12) << "Time taken: " << right << left <<  setw(37) << to_string(duration.count()) +  " seconds" << "│" << right << endl;
+        cout << "│" << setw(53) << "│" << endl;
+        for(auto v: data_.getBestTour()) {
+            cout << "│ " << left << "vertex: " << setw(4) << v->getInfo() << right <<setw(40) << "│"  << endl;
 
+        }
+        cout << "│" << setw(53) << right <<"│" << endl;
+        cout << "└──────────────────────────────────────────────────┘" << endl;
+        waitForEnter();
     }
-    cout << "┌─ Backtracking Algorithm  ────────────────────────┐" << endl;
-    cout << "│" << setw(53) << "│" << endl;
-    cout << "│ " << left << setw(12) << "Tour cost: " << right << left << setw(36) << data_.getCost() << right << " │" << endl;
-    cout << "│ " << left << setw(12) << "Tour size: " << right << left << setw(36) << data_.getBestTour().size() << right << " │" << endl;
-    cout << "│ " << left << setw(12) << "Time taken: " << right << left <<  setw(37) << to_string(duration.count()) +  " seconds" << "│" << right << endl;
-    cout << "│" << setw(53) << "│" << endl;
-    for(auto v: data_.getBestTour()) {
-        cout << "│ " << left << "vertex: " << setw(4) << v->getInfo() << right <<setw(40) << "│"  << endl;
-
-    }
-    cout << "│" << setw(53) << right <<"│" << endl;
-    cout << "└──────────────────────────────────────────────────┘" << endl;
-    waitForEnter();
 }
 
 
